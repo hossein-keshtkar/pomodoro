3c3a6ccfc2d3759a4ad728954f9b6269847ed162
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-26 18:50:50 -0400

    Session component is done

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { BiSolidDownArrow, BiSolidUpArrow } from "react-icons/bi";
 
 import "../styles/Session.css";
+import { SESSION } from "../constants/keywords";
 
-const Session = () => {
+const Session = ({ state, dispatch }) => {
   const [isArrowUpHovered, setIsArrowUpHovered] = useState(false);
   const [isArrowDownHovered, setIsArrowDownHovered] = useState(false);
 
@@ -13,6 +14,15 @@ const Session = () => {
 
   const arrowDownSizeHandler = () => {
     setIsArrowDownHovered((prev) => !prev);
+  };
+
+  const sessionIncrementHandler = () => {
+    if (state.session >= 60) return;
+    dispatch({ type: SESSION, payload: state.session + 1 });
+  };
+  const sessionDecrementHandler = () => {
+    if (state.session <= 0) return;
+    dispatch({ type: SESSION, payload: state.session - 1 });
   };
 
   return (
@@ -25,15 +35,17 @@ const Session = () => {
             size={isArrowUpHovered ? 25 : 20}
             onMouseEnter={arrowUpSizeHandler}
             onMouseLeave={arrowUpSizeHandler}
+            onClick={sessionIncrementHandler}
           />
         </div>
-        <h2 id="session-length">25</h2>
+        <h2 id="session-length">{state.session}</h2>
         <div id="session-decrement">
           <BiSolidDownArrow
             size={isArrowDownHovered ? 25 : 20}
             id="session-arrow-down"
             onMouseEnter={arrowDownSizeHandler}
             onMouseLeave={arrowDownSizeHandler}
+            onClick={sessionDecrementHandler}
           />
         </div>
       </div>
