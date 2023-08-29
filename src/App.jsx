18bc5543741784fa70786ec57bb2817b5068469c
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-29 01:30:56 -0400

    useContext added

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,28 @@ import Session from "./components/Session";
 import Timer from "./components/Timer";
 import { reducer } from "./manager/reducer";
 import { initState } from "./data/initState";
+import Context from "./manager/Context";
 
 function App() {
   const [state, dispatch] = React.useReducer(reducer, initState);
+  const [isResetClicked, setIsResetClicked] = React.useState(false);
 
   return (
-    <div className="App">
-      <h1 className="header">{`Every ${state.session}, Take ${state.break}`}</h1>
-      <div className="columns">
-        <Break state={state} dispatch={dispatch} />
-        <Session state={state} dispatch={dispatch} />
+    <Context.Provider value={{ isResetClicked, setIsResetClicked }}>
+      <div className="App">
+        <h1 className="header">{`Every ${state.session}, Take ${state.break}`}</h1>
+        <div className="columns">
+          <Break state={state} dispatch={dispatch} />
+          <Session state={state} dispatch={dispatch} />
+        </div>
+        <Timer state={state} />
+        <Buttons state={state} dispatch={dispatch} />
+        <footer>
+          Development by <em>Hossein Keshtkar</em>
+          <br /> August 2023
+        </footer>
       </div>
-      <Timer state={state} />
-      <Buttons state={state} dispatch={dispatch} />
-      <footer>
-        Development by <em>Hossein Keshtkar</em>
-        <br /> August 2023
-      </footer>
-    </div>
+    </Context.Provider>
   );
 }
 
