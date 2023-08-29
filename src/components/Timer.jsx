25acbe5d7bff86479adf19cb35f8624ed817b37e
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-28 22:22:57 -0400

    audio handled

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,34 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import "../styles/Timer.css";
+import { displayTimeProperly } from "../funcs/displayTimeProperly";
+import { timeDestructurer } from "../funcs/timeDestructurer";
+import beep from "../assets/beep.mp3";
 
 //** when reset btn pressed, timer also needs to be reset
-
 const Timer = ({ state }) => {
   const [sessionTime, setSessionTime] = useState(state.session * 60);
   const [breakTime, setBreakTime] = useState(state.break * 60);
   const [isSessionTime, setIsSessionTime] = useState(true);
-  const [displayMin, setDisplayMin] = useState(0);
-  const [displaySec, setDisplaySec] = useState(0);
+  const [beepPlayTimes, setBeepPlayTimes] = useState(3);
+  const [displayMin, setDisplayMin] = useState(undefined);
+  const [displaySec, setDisplaySec] = useState(undefined);
   const timeForWhat = isSessionTime ? "Session" : "Break";
+  const audioRef = useRef();
 
-  const displayTime = (min, sec) => {
-    const minutes = min < 10 ? "0" + min : min;
-    const seconds = sec < 10 ? "0" + sec : sec;
-
-    return { min: minutes, sec: seconds };
+  const playBeep = () => {
+    if (beepPlayTimes > 0) {
+      audioRef.current.play();
+      setBeepPlayTimes((prev) => prev - 1);
+    } else {
+      setTimeout(() => {
+        setBeepPlayTimes(3);
+      }, 1000);
+    }
   };
 
   const whatIsTimeFor = () => {
     if (sessionTime < 0) {
       setIsSessionTime(false);
-      setSessionTime(state.session * 60);
-      return;
     } else if (breakTime < 0) {
       setIsSessionTime(true);
-      setBreakTime(state.break * 60);
-      return;
     }
+  };
+
+  const resetTime = () => {
+    const sessionStateTime = state.session * 60;
+    const breakStateTime = state.break * 60;
+
+    if (sessionTime < 0) setSessionTime(sessionStateTime);
+    if (breakTime < 0) setBreakTime(breakStateTime);
   };
 
   useEffect(() => {
@@ -44,16 +56,20 @@ const Timer = ({ state }) => {
   }, [state.isRunning, isSessionTime]);
 
   useEffect(() => {
-    const sessionOrBreakMin = isSessionTime
-      ? parseInt(sessionTime / 60)
-      : parseInt(breakTime / 60);
+    const { minutes, seconds } = timeDestructurer(
+      isSessionTime,
+      sessionTime,
+      breakTime
+    );
 
-    const sessionOrBreakSec = isSessionTime ? sessionTime % 60 : breakTime % 60;
+    if (sessionTime === 0 || breakTime === 0) {
+      playBeep();
+    }
 
-    setDisplayMin(sessionOrBreakMin);
-    setDisplaySec(sessionOrBreakSec);
-
+    setDisplayMin(minutes);
+    setDisplaySec(seconds);
     whatIsTimeFor();
+    resetTime();
   }, [sessionTime, breakTime, isSessionTime]);
 
   useEffect(() => {
@@ -61,18 +77,23 @@ const Timer = ({ state }) => {
     setBreakTime(state.break * 60);
   }, [state.session, state.break]);
 
+  useEffect(() => {
+    console.log(beepPlayTimes);
+  }, [beepPlayTimes]);
+
   return (
     <div
       className="timer-container"
       id="timer-label"
       style={{ borderColor: displayMin < 1 && "red" }}
     >
+      <audio src={beep} ref={audioRef} onEnded={playBeep}></audio>
       <h2>{timeForWhat}</h2>
       <br />
       <div id="time-left">
-        <h1>{displayTime(displayMin, displaySec).min}</h1>
+        <h1>{displayTimeProperly(displayMin)}</h1>
         <h1>:</h1>
-        <h1>{displayTime(displayMin, displaySec).sec}</h1>
+        <h1>{displayTimeProperly(displaySec)}</h1>
       </div>
     </div>
   );
